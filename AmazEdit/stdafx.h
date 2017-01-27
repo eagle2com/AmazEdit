d25494e2add27ad52eb942fd9d6630af77e00f08
eagle2com <peter.lichard@gmail.com>
2017-01-27 17:56:07 +0100

    font packing works

--- AmazEdit/stdafx.h
+++ AmazEdit/stdafx.h
@@ -5,10 +5,40 @@
 
 #pragma once
 
-#include "targetver.h"
-
 #include <stdio.h>
+
+#ifdef WIN32
 #include <tchar.h>
+#include <Windows.h>
+#endif
+
+#include <iostream>
+#include <sstream>
+#include <thread>
+#include <chrono>
+#include <map>
+#include <vector>
+#include <fstream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#define GLM_FORCE_PURE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+using namespace std::chrono_literals;
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
 
 
 
